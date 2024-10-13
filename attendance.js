9032bb26fc10650bde1b159896099fde94d8a914
Darkminato1029 <165239894+Darkminato1029@users.noreply.github.com>
2024-10-13 12:06:49 +0530

    Update attendance.js

--- attendance.js
+++ attendance.js
@@ -7,7 +7,7 @@ document.getElementById('studentForm').addEventListener('submit',function(event)
         const currentDate= new Date();
      const formattedDate = `${currentDate.getDate().toString().padStart(2, '0')}/${(currentDate.getMonth() + 1).toString().padStart(2, '0')}/${currentDate.getFullYear()}`;
          
-        const message=`Date: ${formattedDate}\n presentStudents:\n${presentStudents.join('\n') }`;
+        const message=`Date: ${formattedDate}\n\n presentStudents:\n\n${presentStudents.join('\n') }`;
         const botToken='7593207317:AAGgSCeALmDxCzNFrDnffQNvNt5UDUMi1Ac';
         const chatId='-1002447918965';
         const url=`https://api.telegram.org/bot${botToken}/sendMessage?chat_id=${chatId}&text=${encodeURIComponent(message)}`;
